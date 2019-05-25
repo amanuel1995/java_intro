d821f8c9d8e8362a6d4e7fd79f368408277aa71f
Amanuel <amaniye1@live.com>
2019-05-25 02:57:37 -0400

    Hello World in Java

---
+++ hello_world/src/hello_world/hello_wolrd.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package hello_world;
+
+/**
+ * @author amanuel
+ *
+ */
+public class hello_wolrd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// output a greeting to the console
+		System.out.println("Hello Amanuel!");
+		
+	}
+
+}
