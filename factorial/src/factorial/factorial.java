d61c0b5c836507db014221207f9a09186f09a252
Amanuel <amaniye1@live.com>
2019-05-25 03:55:33 -0400

    simplified version; signed int limit is 12 for factorial...

--- factorial/src/factorial/factorial.java
+++ factorial/src/factorial/factorial.java
@@ -13,32 +13,9 @@ public class factorial {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Finding the factorial of 0...");
-
-		int return_val = compute_factorial(0);
-		
-		System.out.print(return_val);
-		System.out.printf("%n");
-		/////////////////////////////////////////////////////
-		System.out.println("Finding the factorial of 1...");
-
-		int return_val2 = compute_factorial(1);
-		
-		System.out.print(return_val2);
-		System.out.printf("%n");
-		/////////////////////////////////////////////////////
-		System.out.println("Finding the factorial of 5...");
-
-		int return_val3 = compute_factorial(5);
-		
-		System.out.print(return_val3);
-		System.out.printf("%n");
-		/////////////////////////////////////////////////////
-		System.out.println("Finding the factorial of 6...");
-		
-		int return_val4 = compute_factorial(6);
-		
-		System.out.print(return_val4);
+		final int NUMS = 12;
+		for(int i = 0; i < NUMS; i++)
+			System.out.println( i + "! is " + compute_factorial(i));
 	}
 	
 	/**
@@ -50,13 +27,13 @@ public class factorial {
 		int factorial_val = 1;
 		
 		// case 1
-		if (n == 0 || n == 1) {
+		if (n == 0) {
 			factorial_val = 1;
 		}
 		
 		else {
 			for (int i=1; i<=n; i++) {
-				factorial_val = factorial_val * i;
+				factorial_val *= i;
 			}
 		}
 		
